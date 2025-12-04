4b54165e4f921f3edc4fd1a78cfd9e7f7cee3d5f
Paulo <paulovrms@gmail.com>
2025-12-03 23:50:32 -0300

    bug fix

--- ewis-react/vite.config.js
+++ ewis-react/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // Optional: Add SCSS options here if needed
+      }
+    }
+  }
 })
